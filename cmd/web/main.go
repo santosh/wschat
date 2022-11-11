069931bc8606a29f5eb9a5d33e67b37d391ebf66
Santosh Kumar <sntshkmr60@gmail.com>
2022-11-11 10:18:45 +0530

    Construct a messaging pipeline

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/santosh/wschat/internal/handlers"
 )
 
 func main() {
 	mux := routes()
+
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
 
 	log.Println("Starting web server on port 8080")
 
